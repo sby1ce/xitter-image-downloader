8a0f250b990120a1d99a4f411cb1a10b25088116
SomeBodY1CE <someonecallsyou@gmail.com>
2025-02-05 15:20:05 +0300

    feat: switch queries based on logged in status

--- src/content.ts
+++ src/content.ts
@@ -9,6 +9,24 @@ export interface ExtResponse {
   poster: string;
 }
 
+interface DomQueries {
+  images: string;
+  poster: string;
+}
+
+// Selects the main post using the reply section as the indicator
+// alt="Image" excludes profile pictures
+const logged: DomQueries = {
+  images:
+    'article:has(+ div[data-testid="inline_reply_offscreen"]) img[class][alt="Image"][draggable="true"]',
+  poster:
+    'article:has(+ div[data-testid="inline_reply_offscreen"]) a > div > span',
+};
+const notLogged: DomQueries = {
+  images: 'img[class][alt="Image"][draggable="true"]',
+  poster: "article a > div > span",
+};
+
 function transformUrl(img: HTMLImageElement): string {
   const url = new URL(img.src);
   url.searchParams.delete("name");
@@ -16,22 +34,25 @@ function transformUrl(img: HTMLImageElement): string {
   return url.toString();
 }
 
-function search(): string[] {
-  // Selects the main post using the reply section as the indicator
-  // alt="Image" excludes profile pictures
-  const images: HTMLImageElement[] = Array.from(
-    document.querySelectorAll(
-      'article:has(+ div[data-testid="inline_reply_offscreen"]) img[class][alt="Image"][draggable="true"]',
+function isLoggedIn(): boolean {
+  // Looks for reply section as an indicator of whether you're logged in
+  return Boolean(
+    document.querySelector(
+      'article:has(+ div[data-testid="inline_reply_offscreen"])',
     ),
+  );
+}
+
+function search(query: string): string[] {
+  const images: HTMLImageElement[] = Array.from(
+    document.querySelectorAll(query),
   );
   const imageData = images.map(transformUrl);
   return imageData;
 }
 
-function findPoster(): string {
-  const name: HTMLSpanElement | null = document.querySelector(
-    "article a > div > span",
-  );
+function findPoster(query: string): string {
+  const name: HTMLSpanElement | null = document.querySelector(query);
   if (!name || !name.textContent) {
     throw new Error("couldn't find name");
   }
@@ -48,9 +69,18 @@ function listen(
   if ((message as Message).action !== "getImages") {
     return;
   }
+  let images: string[];
+  let poster: string;
+  if (isLoggedIn()) {
+    images = search(logged.images);
+    poster = findPoster(logged.poster);
+  } else {
+    images = search(notLogged.images);
+    poster = findPoster(notLogged.poster);
+  }
   sendResponse({
-    images: search(),
-    poster: findPoster(),
+    images,
+    poster,
   } satisfies ExtResponse);
 }
 
